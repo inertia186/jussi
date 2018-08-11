aaa033b395a98671d66168bf77cd85e86c34536c
Anthony Martin <github@martin-studio.com>
2018-08-11 11:21:21 -0700

    This will allow changing the config without a rebuild.

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -2,4 +2,6 @@
 
 cd "$(dirname "$0")/.."
 
-docker run -itp 9000:8080 "$USER/jussi:$(git rev-parse --abbrev-ref HEAD)"
+docker run -v $(pwd ..):/app/configfiles \
+  --env JUSSI_UPSTREAM_CONFIG_FILE=/app/configfiles/DEV_config.json \
+  -itp 9000:8080 "$USER/jussi:$(git rev-parse --abbrev-ref HEAD)"
